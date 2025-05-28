5c1352c004ad65d35d76aa9b7b981b6741655f21
jcailly <jeremy.cailly@hotmail.fr>
2025-05-29 01:07:48 +0200

    nextjs learn AppRouter step 13 done

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,11 +24,16 @@ export async function createInvoice(formData: FormData) {
     });
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
- 
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
+    try {
+        await sql`
+            INSERT INTO invoices (customer_id, amount, status, date)
+            VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+        `;
+    } catch (error) {
+        // We'll log the error to the console for now
+        console.log(error)
+    }
+
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
@@ -44,19 +49,25 @@ export async function updateInvoice(id: string, formData: FormData) {
         status: formData.get('status'),
     });
     const amountInCents = amount * 100;
- 
-    await sql`
-        UPDATE invoices
-        SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
-        WHERE id = ${id}
-    `;
+    
+    try {
+        await sql`
+            UPDATE invoices
+            SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+            WHERE id = ${id}
+        `;
+    } catch (error) {
+        // We'll log the error to the console for now
+        console.log(error)
+    }
+
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices')
 }
 
 export async function deleteInvoice(id: string) {
- 
+
     await sql`
         DELETE FROM invoices
         WHERE id = ${id}
